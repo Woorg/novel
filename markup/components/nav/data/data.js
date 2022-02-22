4d160c9cd51dff5e5ca692dbb2033e57cba8f741
Igor Gorlov <gorlov35@gmail.com>
2022-02-23 01:17:07 +0300

    init

---
+++ markup/components/nav/data/data.js
@@ -0,0 +1,188 @@
+nav: {
+    primaryList: [
+        {
+            title: 'Наши работы',
+            href: '#works',
+            itemClassMod: ''
+        },
+        {
+            title: 'Информация',
+            href: '#info',
+            itemClassMod: ' ',
+            subnavList: [
+                {
+                    title: 'Карта построенных домов',
+                    href: '#map',
+                },
+                {
+                    title: 'Галерея построенных домов',
+                    href: '#gallery',
+                }
+            ]
+        },
+        {
+            title: 'Клиентам',
+            href: '#clients',
+            itemClassMod: ' ',
+            subnavList: [
+                {
+                    title: 'Карта построенных домов',
+                    href: '#map',
+                },
+                {
+                    title: 'Галерея построенных домов',
+                    href: '#gallery',
+                }
+            ]
+        },
+        {
+            title: 'Контакты',
+            href: '#contacts',
+            itemClassMod: '',
+        },
+
+    ],
+
+    footerListNobel: [
+        {
+            title: 'Каталог проектов',
+            href: '#catalog',
+            itemClassMod: ''
+        },
+        {
+            title: 'Карта проектов',
+            href: '#map',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Галерея проектов',
+            href: '#gallery',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Контакты',
+            href: '#contacts',
+            itemClassMod: '',
+        },
+        {
+            title: 'Ипотека',
+            href: '#mortgage',
+            itemClassMod: '',
+        },
+        {
+            title: 'Вопросы и ответы',
+            href: '#faq',
+            itemClassMod: '',
+        },
+
+    ],
+
+    footerListClients: [
+        {
+            title: 'Калькулятор стоимости проекта',
+            href: '#',
+            itemClassMod: ''
+        },
+        {
+            title: 'Индивидуальное проектирование',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Кредит или рассрочка',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Условия работы',
+            href: '#',
+            itemClassMod: '',
+        },
+        {
+            title: 'Зачем нужен проект?',
+            href: '#',
+            itemClassMod: '',
+        },
+        {
+            title: 'Как заказать проект?',
+            href: '#',
+            itemClassMod: '',
+        },
+
+
+    ],
+
+    footerListInfo: [
+        {
+            title: 'О компании',
+            href: '#',
+            itemClassMod: ''
+        },
+        {
+            title: 'Пример проекта',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Цены и сроки',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Условия работы',
+            href: '#',
+            itemClassMod: '',
+        },
+        {
+            title: 'Контроль качества',
+            href: '#',
+            itemClassMod: '',
+        },
+        {
+            title: 'Полезные статьи',
+            href: '#',
+            itemClassMod: '',
+        },
+
+    ],
+
+
+    interestsList: [
+        {
+            title: 'О компании',
+            href: '#',
+            itemClassMod: ''
+        },
+        {
+            title: 'Каталог проектов',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Каталог проектов',
+            href: '#',
+            itemClassMod: ' ',
+
+        },
+        {
+            title: 'Ипотека',
+            href: '#',
+            itemClassMod: '',
+        },
+        {
+            title: 'Полезные советы',
+            href: '#',
+            itemClassMod: '',
+        }
+
+    ],
+
+
+}
